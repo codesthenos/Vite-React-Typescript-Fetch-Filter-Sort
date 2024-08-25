b8c2805ce8f6c98d985cfd89385216268c802a95
codesthenos <codesthenos@gmail.com>
2024-08-25 11:49:09 +0200

    Commit 1: fetch de 100 usuarios mostrando una lista de los nombres

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App () {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api?results=100')
+      .then(res => res.json())
+      .then(jsonData => {
+        setUsers(jsonData.results)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+
+  }, [])
+
   return (
     <>
       <h1>CODESTHENOS</h1>
       <h3>USER FETCH APP</h3>
+      <ul>
+        {
+          users.map(user => (
+            <li key={user.email}>
+              {user.name.last}
+            </li>
+          ))
+        }
+      </ul>
     </>
   )
 }
