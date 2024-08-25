f53377f84cde8706b57cc571f17f452455e97861
codesthenos <codesthenos@gmail.com>
2024-08-25 17:23:06 +0200

    Commit 3: creada la tabla con los 100 usuarios provinientes del fetch

--- src/App.tsx
+++ src/App.tsx
@@ -21,15 +21,40 @@ function App () {
     <>
       <h1>CODESTHENOS</h1>
       <h3>USER FETCH APP</h3>
-      <ul>
-        {
-          users.map(user => (
-            <li key={user.email}>
-              {user.name.last}
-            </li>
-          ))
-        }
-      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Photo</th>
+            <th>Name</th>
+            <th>Surname</th>
+            <th>Country</th>
+          </tr>
+        </thead>
+      
+        <tbody>
+          {
+            users.map(user => (
+              <tr key={user.email}>
+                <td>
+                  <img src={user.picture.thumbnail} />
+                </td>
+
+                <td>
+                  {user.name.first}
+                </td>
+
+                <td>
+                  {user.name.last}
+                </td>
+
+                <td>
+                  {user.location.country}
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
     </>
   )
 }
