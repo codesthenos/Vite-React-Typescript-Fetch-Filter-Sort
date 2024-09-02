56e2c9264f7c10600d4069b1beaf4a5abf7bac48
codesthenos <codesthenos@gmail.com>
2024-09-02 17:54:06 +0200

    Instalamos tanstack/react-query e importamos en main.tsx el QueryClient y QueryClientProvider, para crear un cliente y envolver la app con el provider que recibe como prop el client el cliente creado

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient(/*config*/)
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+
 )
